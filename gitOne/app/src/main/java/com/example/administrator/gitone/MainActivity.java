d34d135e7247932470196a3279b515e15fb02a03
hx <nmx2678@163.com>
2016-06-21 11:06:45 +0800

    android-button-toast

--- gitOne/app/src/main/java/com/example/administrator/gitone/MainActivity.java
+++ gitOne/app/src/main/java/com/example/administrator/gitone/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.administrator.gitone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b_one;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b_one=(Button)findViewById(R.id.B_One);
+        b_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Hello_world",Toast.LENGTH_LONG);
+            }
+        });
+
     }
 }
